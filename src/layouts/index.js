cf0a8d27c85f32f7133f25cb8bd185ed179fc283
Eric Jinks <ericjinks@gmail.com>
2018-06-21 17:32:41 +1000

    Add favicon

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -40,6 +40,27 @@ class Layout extends React.Component {
             rel="stylesheet"
           />
           <link href="/fonts/fonts.css" rel="stylesheet" />
+          <link
+            rel="apple-touch-icon"
+            sizes="180x180"
+            href="/apple-touch-icon.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="32x32"
+            href="/favicon-32x32.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="16x16"
+            href="/favicon-16x16.png"
+          />
+          <link rel="manifest" href="/site.webmanifest" />
+          <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
+          <meta name="msapplication-TileColor" content="#2b5797" />
+          <meta name="theme-color" content="#ffffff" />
         </Helmet>
 
         <Menu active={menuOpen} handleMenuToggle={this.handleMenuToggle} />
