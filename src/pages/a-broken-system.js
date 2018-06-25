6187b3c5cd090a461766a0580a36135073dd269f
Eric Jinks <ericjinks@gmail.com>
2018-06-25 15:26:04 +1000

    Update Broken View content

--- src/pages/a-broken-system.js
+++ src/pages/a-broken-system.js
@@ -24,7 +24,7 @@ class BrokenSystemPage extends React.Component {
       {
         title: 'Emergency Department',
         description:
-          'Do you know what times of day your peak demand hits by season and day of the week?',
+          'Do you know the length of stay required in each of your treatment areas for the ED system to have flow? And is this updated every 5 min based on the today’s variation in demand?',
         image: emergency,
         linkTo: '#',
         popupContent: (
@@ -32,27 +32,37 @@ class BrokenSystemPage extends React.Component {
             <h3>Emergency Department</h3>
             <ul>
               <li>
-                Do you know what times of day your peak demand hits by season
-                and day of the week?
+                Do you know the length of stay required in each of your
+                treatment areas for the ED system to have flow? And is this
+                updated every 5 min based on the today’s variation in demand?
               </li>
               <li>
-                Can you predict how many patients are going to arrive in the
-                next hour?
+                Do you know which patients have exceeded the required length of
+                stay in each area and why? And what actions you can take to
+                resolve this now?
               </li>
               <li>
-                Do you know right now what kinds of conditions your patients
-                have?
+                Are all of your ED escalation pathways automated with real-time
+                text and email alerts? Are your escalations happening before or
+                after ED flow is blocked?
               </li>
               <li>
-                Do you know the key moments you need medical specialists to give
-                judgements?
+                Do you know how many patients are predicted to present to ED in
+                the following hour and for the rest of the day? And if within
+                previous hours you have had higher than expected presentations?
               </li>
               <li>
-                Do you know when patients need to be discharged to sustain flow?
+                Can you easily see the patients waiting for a ward admission or
+                subspecialty review? And are the teams automatically notified
+                via text or email at the minute these requests are made?
               </li>
               <li>
-                Do you know what has to happen in triage to get patients off
-                ambulance stretchers?
+                Can you see today’s inpatient ward discharges and their impact
+                to ED patient flow?
+              </li>
+              <li>
+                Do you know how long it takes to triage patients that arrive by
+                ambulance and how this impacts POST?
               </li>
             </ul>
             <p>
@@ -72,7 +82,7 @@ class BrokenSystemPage extends React.Component {
       {
         title: 'Beds (admitted patients)',
         description:
-          'Do you know every patient admitted to your hospital right now at a glance?',
+          'Can you see every admitted patient in your hospital right now at a glance and which patients are stranded, complex or have waited too long in each ward for the system to have flow?',
         image: bed,
         linkTo: '#',
         popupContent: (
@@ -80,25 +90,27 @@ class BrokenSystemPage extends React.Component {
             <h3>Beds (admitted patients)</h3>
             <ul>
               <li>
-                Do you know every patient admitted to your hospital right now at
-                a glance?
+                Can you see every admitted patient in your hospital right now at
+                a glance and which patients are stranded, complex or have waited
+                too long in each ward for the system to have flow?
               </li>
               <li>
-                Do you know which of them are at the highest risk of having a
-                long stay?
-              </li>
-              <li>
-                Have you got at your fingertips a list of patients who have
-                stayed too long in every ward?
+                Do you know which patients are at the highest risk of having a
+                long stay or most likely to be readmitted? Is this updated every
+                five minutes?
               </li>
               <li>
                 Have you got individual registers for extremely long stays and
                 special cases?
               </li>
-              <li>Have you got automated red to green strategies?</li>
+              <li>Have you got automated red to green and AEP strategies?</li>
               <li>
                 Do you have an information system so teams across the hospital
-                can agree which patients need special attention?
+                can agree which patients need special attention to prioritise
+                care by acuity and system flow?
+              </li>
+              <li>
+                Are you still having frivolous bed meetings or escalations?
               </li>
               <li>
                 Do you know what lengths of stay each ward needs to make the
@@ -122,7 +134,7 @@ class BrokenSystemPage extends React.Component {
       {
         title: 'Surgery',
         description:
-          'Do you know which surgeons have too many patients on the waiting list?',
+          'Do you know how long each doctor’s waitlist should be to ensure every patient is treated in clinically recommended times? Do you know which doctors have heavy and light order books and is this recalculated daily?',
         image: surgery,
         linkTo: '#',
         popupContent: (
@@ -130,35 +142,36 @@ class BrokenSystemPage extends React.Component {
             <h3>Surgery</h3>
             <ul>
               <li>
-                Do you know which surgeons have too many patients on the waiting
-                list?
+                Do you know how long each doctor’s waitlist should be to ensure
+                every patient is treated in clinically recommended times? Do you
+                know which doctors have heavy and light order books and is this
+                recalculated daily?
+              </li>
+              <li>
+                Do you know how booked tomorrow’s theatre sessions are and who
+                are the optimal patients to book with the remaining theatre
+                minutes?
               </li>
               <li>
                 Do you know how many theatre sessions every specialty requires?
-              </li>
-              <li>
-                Do you know the right balance between allocating emergency and
-                elective surgeries?
+                And within each specialty, do you know which doctors have too
+                many or not enough capacity?
               </li>
               <li>
                 Do you know which patients need to booked today to avoid long
                 waits?
               </li>
               <li>
-                Do you know how long each doctor’s waitlist should be to ensure
-                every patient is treated in clinically recommended times?
-              </li>
-              <li>
-                Do you automatically know the operating times for each surgeon
-                for each procedure?
-              </li>
-              <li>
-                Do you know how effective every surgeon and team are in making
-                offers in time?
-              </li>
-              <li>
                 Have you got at your fingertips the utilisation rates of every
-                theatre, doctor and team?
+                theatre, doctor and team - and associated trends?
+              </li>
+              <li>
+                Do you know which patients are currently booked out of turn and
+                the aggregate impact to the system of this?
+              </li>
+              <li>
+                Do you know which doctors have light order books and how many
+                extra new outpatients they could treat without risking NEST?
               </li>
             </ul>
 
@@ -180,7 +193,7 @@ class BrokenSystemPage extends React.Component {
       {
         title: 'Outpatients',
         description:
-          'Do you know how many slots you need each week to meet your demand?',
+          'Do you know how many outpatient appointments you need each week to see your present demand? And do you use this to set your clinic templates or simply do ‘what we’ve always done’?',
         image: patient,
         linkTo: '#',
         popupContent: (
@@ -188,44 +201,52 @@ class BrokenSystemPage extends React.Component {
             <h3>Outpatients</h3>
             <ul>
               <li>
-                Do you know how many slots you need each week to meet your
-                demand?
+                Do you know how many outpatient appointments you need each week
+                to see your present demand? And do you use this to set your
+                clinic templates or simply do ‘what we’ve always done’?
               </li>
               <li>
-                Do you know if referrals are rising or falling for each
-                specialty?
+                Do you know how many unbooked outpatient slots you have today,
+                tomorrow and in three weeks?
               </li>
-              <li>
-                Do you know how many new and review appointments each doctor
-                does in each clinic?
-              </li>
-              <li>Do you know how many book slots you have had this week?</li>
               <li>
                 Do you know how many unbooked slots were wasted last week and
                 the weeks before?
               </li>
               <li>
-                Do you know how many slots you need in the next four weeks to
-                ensure demand is met?
+                Do you know if referral demand is rising or falling for each
+                specialty? And are you automatically notified if last week had
+                higher than usual demand?
               </li>
               <li>
-                Do you know which teams are treating patients in chronological
-                order?
+                Do you know at a click how many referrals are yet to be
+                categorised? And do you know the longest waiting uncategorised
+                referral?
               </li>
               <li>
-                Do you know which patients need to be seen today to meet your
-                goals?
+                Do you know which patients need to be booked or seen today to
+                meet your goals?
               </li>
               <li>
-                Do you know how many emergency slots you need before planning
-                elective sessions?
+                Do you know how many new and review appointments each doctor did
+                in each clinic last week?
               </li>
               <li>
-                Do you know how many referrals you are yet to even categorise?
+                Can you see the variation in discharges, review appointment
+                generation, conversions to elective surgery and FTAs within
+                doctors of the same team last week?
               </li>
               <li>
-                Do you know if any teams have large sets of uncategorised
-                referrals?
+                Do you know how many emergency slots you need to plan for in
+                next week’s schedule?
+              </li>
+              <li>
+                Do you know how many patients with a booked review appointment
+                could be safely discharged to a GP?
+              </li>
+              <li>
+                Do you know how many patients are currently booked out of
+                chronological order and the aggregate impact of this?
               </li>
             </ul>
 
