cce0a96121b700753a4617363d3f66268f46eae8
Eric Jinks <ericjinks@gmail.com>
2018-06-26 15:07:32 +1000

    Update VideoSection controls

--- src/components/VideoSection.js
+++ src/components/VideoSection.js
@@ -3,16 +3,40 @@ import React from 'react'
 import Plyr from './Plyr'
 import './VideoSection.css'
 
-export default ({ children, videoUrl }) => (
-  <div className="VideoSection">
-    <div className="VideoSection--Content">{children}</div>
-    <div className="VideoSection--Video">
-      <Plyr
-        type="vimeo"
-        videoId={videoUrl}
-        hideControls
-        controls={['play-large']}
-      />
-    </div>
-  </div>
-)
+export default class VideoSection extends React.Component {
+  state = {
+    touched: false
+  }
+
+  render() {
+    const { children, videoUrl } = this.props
+    const { touched } = this.state
+
+    const controls = [
+      'play-large',
+      'play',
+      'progress',
+      'current-time',
+      'mute',
+      'volume',
+      'captions',
+      'airplay',
+      'fullscreen'
+    ]
+
+    return (
+      <div className="VideoSection">
+        <div className="VideoSection--Content">{children}</div>
+        <div className={`VideoSection--Video ${touched ? 'touched' : ''}`}>
+          <Plyr
+            type="vimeo"
+            videoId={videoUrl}
+            hideControls
+            controls={controls}
+            onPlay={() => this.setState({ touched: true })}
+          />
+        </div>
+      </div>
+    )
+  }
+}
