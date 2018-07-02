cb4a6c95ff9cf884578972b57b9784e0d415ac55
Eric Jinks <ericjinks@gmail.com>
2018-07-03 09:34:29 +1000

    Add Popup Scroll-lock

--- src/components/Popup.js
+++ src/components/Popup.js
@@ -4,21 +4,32 @@ import X from 'react-feather/dist/icons/x'
 import './Popup.css'
 
 export default class Popup extends React.Component {
+  state = {
+    popped: true
+  }
+
+  updateBodyStyle = () => {
+    if (typeof window === 'undefined') return false
+    document.body.style.position = this.state.popped ? 'fixed' : 'initial'
+  }
+
+  handleClose = e => {
+    e.preventDefault()
+    document.body.style.position = 'initial'
+    this.setState({ popped: false })
+    this.props.onClose()
+  }
+
   render() {
-    const { children, onClose } = this.props
+    const { children } = this.props
+
+    this.updateBodyStyle()
 
     return (
       <div className="Popup--Wrap">
-        <div className="Popup--Overlay" onClick={onClose} />
+        <div className="Popup--Overlay" onClick={this.handleClose} />
         <div className="container skinny relative">
-          <a
-            href="#"
-            onClick={e => {
-              e.preventDefault()
-              onClose()
-            }}
-            className="Popup--CloseButton"
-          >
+          <a href="#" onClick={this.handleClose} className="Popup--CloseButton">
             <X />
           </a>
           <div className="Popup">{children}</div>
