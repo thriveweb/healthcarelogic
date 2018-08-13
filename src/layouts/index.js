1b7bb2d274e673cf50eb29ba0608460301c68be6
Sam <sam@thriveweb.com.au>
2018-08-13 17:04:27 +1000

    merge 'index.js' 'globalStyles.css' and update 'global.yml'

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -4,45 +4,70 @@ import Helmet from 'react-helmet'
 import 'modern-normalize/modern-normalize.css'
 
 import './globalStyles.css'
-import Meta from '../components/Meta'
-import Nav from '../components/Nav'
+import Header from '../components/Header'
 import Footer from '../components/Footer'
-import GithubCorner from '../components/GithubCorner'
+import Menu from '../components/Menu'
 
-export default ({ children, data }) => {
-  const { siteTitle, siteUrl, socialMediaCard, headerScripts } =
-    data.settingsYaml || {}
-  return (
-    <Fragment>
-      <Helmet defaultTitle={siteTitle} titleTemplate={`%s | ${siteTitle}`}>
-        {/* Add font link tags here */}
-      </Helmet>
+class Layout extends React.Component {
+  state = {
+    menuOpen: false
+  }
 
-      <Meta
-        headerScripts={headerScripts}
-        absoluteImageUrl={
-          socialMediaCard &&
-          socialMediaCard.image &&
-          siteUrl + socialMediaCard.image
-        }
-      />
+  handleMenuToggle = e => {
+    // if (e) e.preventDefault()
+    this.setState({ menuOpen: !this.state.menuOpen })
+  }
 
-      <GithubCorner url="https://github.com/thriveweb/whitesmoke" />
+  updateBodyStyle = () => {
+    if (typeof window === 'undefined') return false
+    // Lock body scroll
+    document.body.style.position = this.state.menuOpen ? 'fixed' : 'initial'
+  }
 
-      <Nav />
+  render() {
+    const { children, data } = this.props
+    const { menuOpen } = this.state
+    const {
+      siteTitle,
+      siteDescription,
+      email,
+      phone,
+      siteUrl,
+      socialMediaCard,
+      headerScripts
+    } = data.settingsYaml || {}
 
-      <Fragment>{children()}</Fragment>
+    return (
+      <Fragment>
+        <Helmet defaultTitle={siteTitle} titleTemplate={`%s | ${siteTitle}`}>
+          {/* Add font link tags here */}
+        </Helmet>
 
-      <Footer />
-    </Fragment>
-  )
+        <Menu active={menuOpen} handleMenuToggle={this.handleMenuToggle} />
+
+        <Header
+          siteTitle={siteTitle}
+          handleMenuToggle={this.handleMenuToggle}
+          menuOpen={menuOpen}
+        />
+
+        <Fragment>{children()}</Fragment>
+
+        <Footer siteTitle={siteTitle} siteEmail={email} sitePhone={phone} />
+      </Fragment>
+    )
+  }
 }
+
+export default Layout
 
 export const query = graphql`
   query IndexLayoutQuery {
     settingsYaml {
       siteTitle
       siteDescription
+      email
+      phone
       headerScripts
       socialMediaCard {
         image
