d364e2743d01233ccfacfed0ae12369f10ee2175
Sam <sam@thriveweb.com.au>
2018-08-13 18:10:23 +1000

    merge 'AboutPage.js'

--- src/templates/AboutPage.js
+++ src/templates/AboutPage.js
@@ -4,43 +4,93 @@ import Helmet from 'react-helmet'
 import PageHeader from '../components/PageHeader'
 import Image from '../components/Image'
 import Content from '../components/Content.js'
+
+import Link from '../components/Link'
+import BackgroundImage from '../components/BackgroundImage'
+import TeamGrid from '../components/TeamGrid'
+import bgEmblem3d from '../images/bg-emblem-3d-white.svg'
+
 import './AboutPage.css'
 
-// Export Template for use in CMS preview
-export const AboutPageTemplate = ({
-  title,
-  subtitle,
-  featuredImage,
-  section1,
-  section2,
-  testImage,
-  body
-}) => (
-  <main className="About">
-    <Helmet>
-      <title>{title}</title>
-    </Helmet>
-    <PageHeader
-      title={title}
-      subtitle={subtitle}
-      backgroundImage={featuredImage}
-    />
+class AboutPageTemplate extends React.Component {
+  render() {
+    let { title, subtitle, featuredImage, body } = this.props
+    return (
+      <main>
+        <Helmet>
+          <title>Our story</title>
+        </Helmet>
 
-    <section className="section">
-      <div className="container">
-        <Content source={section1} />
-      </div>
-    </section>
+        <section className="section dark thick vh-100">
+          <BackgroundImage
+            src={bgEmblem3d}
+            contain
+            animate
+            opacity={0.4}
+            style={{ top: '20rem', bottom: '20rem' }}
+          />
 
-    <section className="section">
-      <div className="container">
-        <Content source={section2} />
-        <p>The image below is a {'<Image />'}</p>
-        <Image src={testImage} alt="Image" />
-      </div>
-    </section>
-  </main>
-)
+          <div className="container skinny">
+            <h1>Our story</h1>
+            <div className="About--Intro">
+              <p className="statement">
+                Healthcare Logic is committed to developing software that helps
+                clinical and managerial leaders improve the performance of their
+                hospitals.
+              </p>
+              <p>
+                Having worked in strategic health improvement for almost two
+                decades across five countries and three continents, founder and
+                CEO Martin Connor is driven by the remarkable fact there is no
+                international standard definition of the appropriate data
+                required to operationally manage a hospital.
+              </p>
+              <p>
+                Our software has changed that and is already being used in more
+                than 50 hospitals where it is optimising performance in areas
+                such as Outpatients, Surgery and Theatres, Endoscopy, Emergency
+                Departments and Inpatient Bed Management.
+              </p>
+              <p>
+                Too much hospital reporting is not interesting, let alone
+                important. The question we constantly ask ourselves is - can we
+                get an action out of the data we are producing? If the answer is
+                no, we throw it out. If it is yes, we keep it and get ready to
+                take action.
+              </p>
+              <p>
+                In search of a solution to many of the global challenges facing
+                hospitals, we have created a common language that is helping
+                solve the dilemma of capacity, demand and process and showing
+                there is a simpler, cheaper and better way.
+              </p>
+            </div>
+            <Link
+              to="/a-case-for-change/"
+              strong
+              icon="page"
+              arrow="down"
+              scrollButton
+            >
+              See. Change.
+            </Link>
+          </div>
+        </section>
+
+        <section
+          className="section primary thick"
+          data-scrollToTarget
+          id="team"
+        >
+          <div className="container skinny">
+            <h2 style={{ marginBottom: '4rem' }}>Team</h2>
+            <TeamGrid />
+          </div>
+        </section>
+      </main>
+    )
+  }
+}
 
 const AboutPage = ({ data: { page } }) => (
   <AboutPageTemplate {...page} {...page.frontmatter} body={page.html} />
