1c895976498dbdfdada78b2a62f639c5c50fd2f1
Samantha <sam@thriveweb.com.au>
2018-08-15 11:30:10 +1000

    update helmet

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -40,7 +40,63 @@ class Layout extends React.Component {
     return (
       <Fragment>
         <Helmet defaultTitle={siteTitle} titleTemplate={`%s | ${siteTitle}`}>
-          {/* Add font link tags here */}
+          <meta
+            name="viewport"
+            content="width=device-width, initial-scale=1.0, viewport-fit=cover"
+          />
+          <meta name="apple-mobile-web-app-capable" content="yes" />
+          <meta
+            name="apple-mobile-web-app-status-bar-style"
+            content="black-translucent"
+          />
+
+          <meta
+            name="msvalidate.01"
+            content="B0112812CA622A13C327AF572E2BA5B5"
+          />
+          <meta
+            name="google-site-verification"
+            content="u7PuEsbocVgyS2ZclTrpzmYZJJB7B_5Ji4wVEK9TMx0"
+          />
+          <link
+            href="https://fonts.googleapis.com/css?family=Work+Sans:300,400"
+            rel="stylesheet"
+          />
+          <link href="/fonts/fonts.css" rel="stylesheet" />
+          <link
+            rel="apple-touch-icon"
+            sizes="180x180"
+            href="/apple-touch-icon.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="32x32"
+            href="/favicon-32x32.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="16x16"
+            href="/favicon-16x16.png"
+          />
+          <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
+          <meta name="msapplication-TileColor" content="#2b5797" />
+          <meta name="theme-color" content="#ffffff" />
+
+          {/* Global site tag (gtag.js) - Google Analytics */}
+          <script
+            async
+            src="https://www.googletagmanager.com/gtag/js?id=UA-112589638-13"
+          />
+          <script>
+            {`
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments)}
+              gtag('js', new Date());
+              gtag('config', 'UA-112589638-13');
+              `}
+          </script>
         </Helmet>
 
         <Menu active={menuOpen} handleMenuToggle={this.handleMenuToggle} />
