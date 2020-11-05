0dff3b1f9996ea8d8829b766e008eaa004b3ffa0
deanoakley <dean@thriveweb.com.au>
2020-11-05 13:19:47 +1000

    init new work page

---
+++ content/pages/work.md
@@ -0,0 +1,56 @@
+---
+template: WorkPage
+slug: work-with-us
+title: Work with us
+section1:
+  title: Join our team
+  subtitle: |-
+    Healthcare Logic is committed to developing software that helps clinical and managerial leaders improve the performance of their hospitals.
+  content: |-
+    Having worked in strategic health improvement for almost two decades across five countries and three continents, founder and CEO Martin Connor is driven by the remarkable fact there is no international standard definition of the appropriate data required to operationally manage a hospital.
+
+
+    Our software has changed that and is already being used in more than 60 hospitals where it is optimising performance in areas such as Outpatients, Surgery and Theatres, Endoscopy, Emergency Departments and Inpatient Bed Management.
+
+
+    Too much hospital reporting is not interesting, let alone important. The question we constantly ask ourselves is - can we get an action out of the data we are producing? If the answer is no, we throw it out. If it is yes, we keep it and get ready to take action.
+
+
+    In search of a solution to many of the global challenges facing hospitals, we have created a common language that is helping solve the dilemma of capacity, demand and process and showing there is a simpler, cheaper and better way.
+
+board:
+  - title: Andy Hill
+    position: Board Chair and Director
+    image: '/images/team/andy-hill.jpg'
+    slug: ../team/andy-hill
+  - title: 'Brett Thebault'
+    position: 'Board Director'
+    image: '/images/team/brett-thebault.jpg'
+    slug: ../team/brett-thebault
+  - title: 'Suzanne Yeates'
+    position: 'Company Secretary'
+    image: '/images/team/suzanne-yeates.jpg'
+    slug: ../team/suzanne-yeates
+
+leadership:
+  - title: 'Professor Martin Connor'
+    position: 'Founder and CEO'
+    image: '/images/team/martin-connor.jpg'
+    slug: ../team/martin-connor
+    order: 1
+  - title: 'Kate Whylie'
+    position: 'Director of Customer Success'
+    image: '/images/team/kate-whylie.jpg'
+    slug: ../team/kate-whylie
+    order: 2
+  - title: 'Associate Professor Christopher Ogg'
+    position: 'Director Research and Development'
+    image: '/images/team/christopher-ogg.jpg'
+    slug: ../team/christopher-ogg
+    order: 3
+  - title: Andrew Ge-Hall
+    position: Director Software Development
+    image: '/images/uploads/team/andrew-ge-hall.jpg'
+    slug: ../team/andrew-ge-hall
+    order: 4
+---
---
+++ src/templates/WorkPage.js
@@ -0,0 +1,147 @@
+import React from 'react'
+import Helmet from 'react-helmet'
+
+import Link from '../components/Link'
+import Content from '../components/Content'
+import BackgroundImage from '../components/BackgroundImage'
+import TeamGrid from '../components/TeamGrid'
+import bgEmblem3d from '../images/bg-emblem-3d-white.svg'
+import Meta from '../components/Meta'
+
+import './WorkPage.css'
+
+export class WorkPageTemplate extends React.Component {
+  render() {
+    let { title, section1, board, leadership, meta } = this.props
+    return (
+      <main>
+        <Helmet>
+          <title>{title}</title>
+        </Helmet>
+        <Meta {...meta} />
+
+        {section1 && (
+          <section className="section dark thick vh-100">
+            <BackgroundImage
+              src={bgEmblem3d}
+              contain
+              animate
+              opacity={0.4}
+              style={{ top: '20rem', bottom: '20rem' }}
+            />
+
+            <div className="container skinny">
+              <h1>{section1.title}</h1>
+              <div className="About--Intro">
+                <p className="statement">{section1.subtitle}</p>
+                <Content src={section1.content} />
+              </div>
+              <Link to="#team" strong icon="page" arrow="down" scrollButton>
+                Meet the Team
+              </Link>
+            </div>
+          </section>
+        )}
+
+        <section className="section primary thick" id="team">
+          <div className="container skinny">
+            <h2 style={{ marginBottom: '4rem' }}>The Board</h2>
+            <TeamGrid team={board} />
+            <br />
+            <br />
+            <h2 style={{ marginBottom: '4rem' }}>Leadership Team</h2>
+            <TeamGrid team={leadership} />
+          </div>
+
+          <Link to="/contact/" strong icon="page" arrow="right" scrollButton>
+            Contact us
+          </Link>
+        </section>
+      </main>
+    )
+  }
+}
+
+const WorkPage = ({ data: { page, board, leadership } }) => (
+  <WorkPageTemplate
+    {...page}
+    {...page.frontmatter}
+    body={page.html}
+    board={board.edges.map(post => ({
+      ...post.node,
+      ...post.node.frontmatter,
+      ...post.node.fields
+    }))}
+    leadership={leadership.edges.map(post => ({
+      ...post.node,
+      ...post.node.frontmatter,
+      ...post.node.fields
+    }))}
+  />
+)
+
+export default WorkPage
+
+export const pageQuery = graphql`
+  query WorkPage($id: String!) {
+    page: markdownRemark(id: { eq: $id }) {
+      html
+      frontmatter {
+        title
+        section1 {
+          title
+          subtitle
+          content
+        }
+        meta {
+          title
+          description
+          noindex
+          canonicalLink
+        }
+      }
+    }
+    board: allMarkdownRemark(
+      filter: { fields: { contentType: { eq: "board" } } }
+    ) {
+      edges {
+        node {
+          fields {
+            slug
+            contentType
+          }
+          frontmatter {
+            title
+            order
+            image {
+              ...NoBlurImage
+            }
+            position
+            description
+          }
+        }
+      }
+    }
+    leadership: allMarkdownRemark(
+      filter: { fields: { contentType: { eq: "leadership" } } }
+    ) {
+      edges {
+        node {
+          fields {
+            slug
+            contentType
+          }
+          frontmatter {
+            title
+            order
+            image {
+              ...NoBlurImage
+            }
+            position
+            description
+          }
+        }
+      }
+    }
+  }
+`
