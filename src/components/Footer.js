dbb6903ff544716df74451ec0ae952cf7037390a
Samantha Scott <sam@thriveweb.com.au>
2022-04-19 15:53:59 +1000

    update privacy policy links

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,7 +6,7 @@ import SubscribeForm from '../components/SubscribeForm'
 import Link from './Link'
 import ScrollToTop from './ScrollToTop'
 import emblem from '../images/emblem.svg'
-import privacyPolicy from '../../images/uploads/hcl_website-privacy-policy.pdf'
+import privacyPolicy from 'https://www.healthcarelogic.com/images/uploads/hcl_website-privacy-policy.pdf'
 
 import './Footer.css'
 
